0c22c40af30808851353a0dbe532e4d156fb6a2d
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-09-05 13:18:53 +0300

    wifi: ath11k: implement QRTR endpoint ID fetching for PCI
    QRTR endpoint ID fetching for PCIe devices will use MHI.
    
    Originally-by: Mihai Moldovan <ionic@ionic.de>
    Signed-off-by: Mihai Moldovan <ionic@ionic.de>
    Signed-off-by: Juha-Matti Tilli <juha-matti.tilli@iki.fi>

--- drivers/net/wireless/ath/ath11k/mhi.h
+++ drivers/net/wireless/ath/ath11k/mhi.h
@@ -27,4 +27,5 @@ void ath11k_mhi_clear_vector(struct ath11k_base *ab);
 int ath11k_mhi_suspend(struct ath11k_pci *ar_pci);
 int ath11k_mhi_resume(struct ath11k_pci *ar_pci);
 
+int ath11k_mhi_set_qrtr_endpoint_id(struct ath11k_base *ab);
 #endif
